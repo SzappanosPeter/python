e86a9901e1270da179024eda349ce9007d3f5331
SzappanosPeter <123642370+SzappanosPeter@users.noreply.github.com>
2023-01-27 15:22:19 +0100

    Add files via upload

---
+++ python.py
@@ -0,0 +1,13 @@
+szam = int(input('Adj meg egy számot! '))
+szam2 = int(input('Adj meg egy számot! '))
+if szam < szam2:
+    print(szam2)
+
+elif szam == szam2:
+    print('A két szám egyenlő!')
+
+else:   
+    print(szam)
+
+
+
